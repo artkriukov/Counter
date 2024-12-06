c8d10cddfd816f2a1e24066f27504f91c45af189
Artem Kriukov <art.kriukov@gmail.com>
2024-12-06 19:39:23 +0300

    Добавлена кнопка и лейбл, реализована логика изменения значения лейбла при нажатии на кнопку

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var counterUILabel: UILabel!
+    @IBOutlet weak var incrementUIButton: UIButton!
+    
+    var count = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        incrementUIButton.layer.cornerRadius = 20
     }
 
+    @IBAction func increment(_ sender: UIButton) {
+        count += 1
+        counterUILabel.text = "Значение счётчика: \(count)"
+    }
 
 }
 
