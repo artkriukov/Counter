4c73458e3cc0f67c85fdca4c57823e12421572ec
Artem Kriukov <art.kriukov@gmail.com>
2024-12-07 12:42:11 +0300

    - Добавлены кнопки «+», «-» и обнуления:   - Кнопка «+» (красная): увеличивает значение счётчика на 1.   - Кнопка «-» (синяя): уменьшает значение счётчика на 1 (не ниже 0).   - Кнопка обнуления: сбрасывает значение счётчика на 0. - Добавлено текстовое поле  для отображения истории изменений:   - История фиксирует успешные изменения, сброс, и попытки уменьшить счётчик ниже 0.   - Формат истории: . - Реализована логика обновления счётчика и записи истории.

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -10,19 +10,58 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var counterUILabel: UILabel!
     @IBOutlet weak var incrementUIButton: UIButton!
+    @IBOutlet weak var decrementUIButton: UIButton!
+    @IBOutlet weak var resetUIButton: UIButton!
+    @IBOutlet weak var historyTextView: UITextView!
     
     var count = 0
+    var history = "История изменений: \n"
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        incrementUIButton.layer.cornerRadius = 20
+        incrementUIButton.layer.cornerRadius = 10
+        decrementUIButton.layer.cornerRadius = 10
+        resetUIButton.layer.cornerRadius = 10
+        historyTextView.text = history
     }
 
     @IBAction func increment(_ sender: UIButton) {
         count += 1
-        counterUILabel.text = "Значение счётчика: \(count)"
+        logHistory(message: "значение изменено на +1 \n")
+        updateCount()
     }
 
+    @IBAction func decrement(_ sender: UIButton) {
+        if count > 0 {
+            count -= 1
+            logHistory(message: "значение изменено на -1 \n")
+            updateCount()
+        } else {
+            logHistory(message: "попытка уменьшить значение счётчика ниже 0 \n")
+        }
+    }
+    @IBAction func reset(_ sender: UIButton) {
+        count = 0
+        logHistory(message: "значение сброшено \n")
+        updateCount()
+    }
+    
+    func updateCount() {
+        counterUILabel.text = "Значение счётчика: \(count)"
+    }
+    
+    func logHistory(message: String) {
+        let date = DateFormatter()
+        date.dateFormat = "dd-MM-yyyy HH:mm:ss"
+        let timestamp = date.string(from: Date())
+        
+        history += "[\(timestamp)]: \(message)"
+        updateHistory()
+    }
+    
+    func updateHistory() {
+        historyTextView.text = history
+    }
 }
 
